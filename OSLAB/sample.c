91a73c5ba4a604f7d88b524f7ce2b43b8b480882
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-08-30 12:16:35 +0530

    N Jyothi Created Sample.c

---
+++ OSLAB/sample.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: N.Jyothi
+   Roll No: 204G1A0546
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
