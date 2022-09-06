872125c47d6e1b576374ec213b116e8a93969266
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-09-06 11:39:47 +0530

    update FCFS.c

--- OSLAB/FCFS.c
+++ OSLAB/FCFS.c
@@ -1,10 +1,9 @@
-\
-
-
-
-
-
-
+/* Name Of the Candidate: N.Jyothi
+   Roll No: 204G1A0546
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
 
 #include<stdio.h>
 #include<conio.h>
