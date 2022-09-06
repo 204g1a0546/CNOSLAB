fd6b4d8c72548d8166b4856f030b4d13c6411136
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-09-06 11:42:00 +0530

    create SJF.c

---
+++ OSLAB/SJF.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: N.Jyothi
+   Roll No: 204G1A0546
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-0-2022
+*/
