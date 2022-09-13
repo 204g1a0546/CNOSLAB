4dd09157481508a190132e2e9c5882cd5c1f19af
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-09-13 10:45:40 +0530

    Update Priority.c

--- OSLAB/Priority.c
+++ OSLAB/Priority.c
@@ -1,6 +1,6 @@
 /* Name Of the Candidate: N.Jyothi
    Roll No: 204G1A0546
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Title of the Experiment: CPU Scheduling Using Priority Algorithm
    Date of Creation: 06-09-2022
    Date of Execution: 06-09-2022
 */
