516bca358edd6f0ce76c078239765ce62e1200b2
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-09-13 10:44:33 +0530

    Update RR.c

--- OSLAB/RR.c
+++ OSLAB/RR.c
@@ -1,8 +1,8 @@
 /* Name Of the Candidate: N.Jyothi
    Roll No: 204G1A0546
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
-   Date of Creation: 06-09-2022
-   Date of Execution: 06-09-2022
+   Title of the Experiment: CPU Scheduling Using RR Algorithm
+   Date of Creation: 13-09-2022
+   Date of Execution: 13-09-2022
 */
 
 #include<stdio.h>
