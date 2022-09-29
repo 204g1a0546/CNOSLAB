4465c4ee9a724f1cedc69b78b24461d588280a88
204g1a0546 <204g1a0546@srit.ac.in>
2022-09-29 14:32:30 +0530

    First list of Errors During Compilation.

--- OSLAB/SJF.c
+++ OSLAB/SJF.c
@@ -6,12 +6,12 @@
 */
 
 #include<stdio.h>
-#include<conio.h>
-main()
+
+void main()
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
 tatavg;
-clrscr();
+
 printf("\nEnter the number of processes -- ");
 
 scanf("%d", &n);
@@ -45,5 +45,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n); 
-getch();
+getc();
 }
