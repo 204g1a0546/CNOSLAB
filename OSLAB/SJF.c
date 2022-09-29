989ed3abe547d0755b96ade3365da3d15b9250e3
204g1a0546 <204g1a0546@srit.ac.in>
2022-09-29 14:41:36 +0530

    My first SJF.c is successfully completed

--- OSLAB/SJF.c
+++ OSLAB/SJF.c
@@ -7,7 +7,7 @@
 
 #include<stdio.h>
 
-void main()
+int main()
 {
 int p[20], bt[20], wt[20], tat[20], i, k, n, temp; float wtavg,
 tatavg;
@@ -45,5 +45,5 @@ for(i=0;i<n;i++)
 printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
 printf("\nAverage Waiting Time -- %f", wtavg/n);
 printf("\nAverage Turnaround Time -- %f", tatavg/n); 
-getch();
+return 0;
 }
