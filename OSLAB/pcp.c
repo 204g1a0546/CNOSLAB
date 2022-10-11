d2f9c6f6cdf534f7fdd4c1bba202f73d26af418d
204G1A0546 <204G1A0546@srit.ac.in>
2022-10-11 12:06:57 +0530

    First list of Errors During Compilation.

--- OSLAB/pcp.c
+++ OSLAB/pcp.c
@@ -9,8 +9,7 @@
 #include<stdio.h>
 void main()
 {
-int buffer[10], bufsize, in, out, produce, consume,
-choice=0; in = 0;
+int buffer[10], bufsize, in, out, produce, consume,choice=0; in = 0;
 out = 0;
 bufsize = 10;
 while(choice !=3)
@@ -24,13 +23,12 @@ case 1: if((in+1)%bufsize==out)
 printf(“\nBuffer is Full”);
 else
 {
-break;
-}
 printf(“\nEnter the value: “);
 scanf(“%d”, &produce);
 buffer[in] = produce;
 in = (in+1)%bufsize;
 break;
+}
 case 2: if(in == out)
 printf(“\nBuffer is Empty”);
 else
