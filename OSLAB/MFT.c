982b86bf72cdf7a3f4dc5b2681464d0ffb90a92c
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-10-11 11:43:29 +0530

    Create MFT.c

---
+++ OSLAB/MFT.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: N.Jyothi
+   Roll No: 204G1A0546
+   Title of the Experiment: MEMORY MANAGEMENT WITH FIXED PARTITIONING TECHNIQUE(MFT)
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
