0594c12d3a776c37cf4aecc3442212de2b4e50bf
204G1A0546 <204G1A0546@srit.ac.in>
2022-10-11 12:16:32 +0530

    First list of Errors rectified

--- OSLAB/pcp.c
+++ OSLAB/pcp.c
@@ -14,27 +14,27 @@ out = 0;
 bufsize = 10;
 while(choice !=3)
 {
-printf(“\n 1. Produce \t 2. Consume \t3. Exit”);
-printf(“\n Enter your choice: ”);
-scanf(“%d”,&choice);
+printf("\n 1. Produce \t 2. Consume \t3. Exit");
+printf("\n Enter your choice: ");
+scanf("%d",&choice);
 switch(choice) {
 
 case 1: if((in+1)%bufsize==out)
-printf(“\nBuffer is Full”);
+printf("\nBuffer is Full");
 else
 {
-printf(“\n Enter the value: “);
-scanf(“%d”, &produce);
+printf("\n Enter the value: ");
+scanf("%d", &produce);
 buffer[in] = produce;
 in = (in+1)%bufsize;
 break;
 }
 case 2: if(in == out)
-printf(“\n Buffer is Empty”);
+printf("\n Buffer is Empty");
 else
 {
 consume = buffer[out];
-printf(“\n The consumed value is %d”, consume);
+printf("\n The consumed value is %d", consume);
 out = (out+1)%bufsize;
 }
 break;
