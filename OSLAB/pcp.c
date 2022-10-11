041f97283075a2a6aadadb3bdba242198b2df03a
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-10-11 10:45:19 +0530

    created pcp.c

---
+++ OSLAB/pcp.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: N.Jyothi
+   Roll No: 204G1A0546
+   Title of the Experiment: Producer Consumer Problem
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
