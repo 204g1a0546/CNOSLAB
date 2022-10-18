6a445275b681a27ea660eb47a77703f510053474
JYOTHI NOSSAM <94515119+204g1a0546@users.noreply.github.com>
2022-10-18 12:00:25 +0530

    Create DPP.c

---
+++ OSLAB/DPP.c
@@ -0,0 +1,7 @@
+/* Name Of the Candidate: N.Jyothi
+   Roll No: 204G1A0546
+   Title of the Experiment: MEMORY VARIABLE PARTIONING TYPE (MVT)
+   Date of Creation: 18-10-2022
+   Date of Execution: 18-10-2022
+*/
+
